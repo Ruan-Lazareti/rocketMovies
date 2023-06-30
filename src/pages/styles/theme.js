b724f1d54f4dbe4db713a9c7a41570195ce3c6e0
ruan <ruanlazareti@hotmail.com>
2023-06-30 12:02:49 -0300

    project created with the basic structure

---
+++ src/pages/styles/theme.js
@@ -0,0 +1,14 @@
+export default {
+  COLORS: {
+    BACKGROUND_900: "#1C1B1E",
+    BACKGROUND_800: "#262529",
+    BACKGROUND_700: "#312E38",
+
+    GRAY_200: "#948F99",
+    GRAY_100: "#CAC4CF",
+    
+    PINK: "#FF859B",
+    WHITE: "#F4EDE8",
+    BLACK: "0D0C0F"
+  }
+}
